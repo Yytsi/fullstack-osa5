804910d9edd16230be0be9cc51a92267d145cce0
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-02-03 21:25:52 +0200

    5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders blog component', async () => {
+test('renders blog component title only', () => {
   const blog = {
     title: 'My blog title',
     user: {
@@ -22,4 +22,32 @@ test('renders blog component', async () => {
   expect(url).toBeNull()
   const likes = screen.queryByText('likes 142')
   expect(likes).toBeNull()
+})
+
+test('renders blog component title only', async () => {
+  const blog = {
+    title: 'My blog title',
+    user: {
+      username: 'user'
+    },
+    url: 'hereurl',
+    likes: 142
+  }
+
+  render(<Blog blog={blog} showRemoveButton={true} setBlogs={() => {}}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+
+  const url = screen.queryByText('hereurl')
+  expect(url).toBeNull()
+  const likes = screen.queryByText('likes')
+  expect(likes).toBeNull()
+
+  await user.click(button)
+
+  const likeText = screen.queryByText('likes', { exact: false })
+  expect(likeText).toBeInTheDocument()
+  const urlText = screen.queryByText('hereurl', { exact: false })
+  expect(urlText).toBeInTheDocument()
 })
