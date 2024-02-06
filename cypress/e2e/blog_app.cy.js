d5db9c466ca7df0eed79ec6518cd42eba3291c9c
Tuukka Yildirim <tuukka.yildirim@gmail.com>
2024-02-06 14:40:02 +0200

    5.18

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+    const user = {
+      name: 'Testi Mies',
+      username: 'testimies',
+      password: 'strongpass42'
+    }
+    cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
     cy.visit('')
   })
 
@@ -8,5 +14,20 @@ describe('Blog app', function() {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('username').find('input').type('testimies')
+      cy.contains('password').find('input').type('strongpass42')
+      cy.contains('login').click()
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('username').find('input').type('testimies')
+      cy.contains('password').find('input').type('wrongpass')
+      cy.contains('login').click()
+      cy.contains('wrong credentials')
+    })
   })
 })
